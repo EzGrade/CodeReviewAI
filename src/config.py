929514026bc37358891904baed700c9392b39d5d
Grade <aborecevskij94@gmail.com>
2024-10-31 17:55:10 +0200

    add: logger values

--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
 from utils.environment import get_env_var
 
-GITHUB_APP_ID = get_env_var("GITHUB_APP_ID")
-GITHUB_PRIVATE_KEY = get_env_var("GITHUB_PRIVATE_KEY")
+GITHUB_APP_ID = int(get_env_var("GITHUB_APP_ID", default=0))
+GITHUB_PRIVATE_KEY = get_env_var("GITHUB_PRIVATE_KEY", default="")
+
+LOGGING_LEVEL = get_env_var("LOGGING_LEVEL", default="DEBUG")
