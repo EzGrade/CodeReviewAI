18216cbe25b26ca1fa655dbc4d65203c1d55e9eb
Grade <aborecevskij94@gmail.com>
2024-10-31 18:34:25 +0200

    add: support for local .pem file

--- src/config.py
+++ src/config.py
@@ -2,5 +2,8 @@ from utils.environment import get_env_var
 
 GITHUB_APP_ID = int(get_env_var("GITHUB_APP_ID", default=0))
 GITHUB_PRIVATE_KEY = get_env_var("GITHUB_PRIVATE_KEY", default="")
+if GITHUB_PRIVATE_KEY.endswith(".pem"):
+    with open(GITHUB_PRIVATE_KEY, "r", encoding="utf-8") as key_file:
+        GITHUB_PRIVATE_KEY = key_file.read()
 
 LOGGING_LEVEL = get_env_var("LOGGING_LEVEL", default="DEBUG")
