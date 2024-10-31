48e282450a3268d19b7d1f9ae837e847a52df05b
Grade <aborecevskij94@gmail.com>
2024-10-31 17:54:01 +0200

    add: logger

--- src/api/review/views.py
+++ src/api/review/views.py
@@ -1,13 +1,17 @@
 from fastapi import APIRouter
+import logging
+
 from api.review.models import Review
 from services.github_service import Github
 from utils.url import extract_repo_from_url
 
 router = APIRouter()
+logger = logging.getLogger(__name__)
 
 
 @router.post(path="/review")
 async def review(request: Review):
+    logger.debug(f"Review request: {request}")
     assignment_description = request.assignment_description
     repo_owner, repo_name = extract_repo_from_url(request.github_repo_url)
     candidate_level = request.candidate_level
