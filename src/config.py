d9bfe928ff3d551dd144196a4b0e4ab82bc07c03
Grade <aborecevskij94@gmail.com>
2024-11-01 13:10:04 +0200

    upd: openai settings, prompt

--- src/config.py
+++ src/config.py
@@ -6,4 +6,11 @@ if GITHUB_PRIVATE_KEY.endswith(".pem"):
     with open(GITHUB_PRIVATE_KEY, "r", encoding="utf-8") as key_file:
         GITHUB_PRIVATE_KEY = key_file.read()
 
+OPENAI_API_KEY = get_env_var("OPENAI_API_KEY", default="")
+OPENAI_MODEL = get_env_var("OPENAI_MODEL", default="gpt-3.5-turbo")
+
+PROMPT_PATH = get_env_var("PROMPT_PATH", default="src/prompt.txt")
+with open(PROMPT_PATH, "r", encoding="utf-8") as prompt_file:
+    PROMPT = prompt_file.read()
+
 LOGGING_LEVEL = get_env_var("LOGGING_LEVEL", default="DEBUG")
