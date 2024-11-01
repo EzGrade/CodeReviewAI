778e6771334a7d249dddf5a53acda1c5488faedb
Grade <aborecevskij94@gmail.com>
2024-11-01 14:53:58 +0200

    upd: better error handling; new variables

--- src/config.py
+++ src/config.py
@@ -1,16 +1,23 @@
-from utils.environment import get_env_var
+from utils.util_environment.environment import get_env_var
 
-GITHUB_APP_ID = int(get_env_var("GITHUB_APP_ID", default=0))
-GITHUB_PRIVATE_KEY = get_env_var("GITHUB_PRIVATE_KEY", default="")
+LOGGING_LEVEL = get_env_var("LOGGING_LEVEL", default="DEBUG")
+
+GITHUB_APP_ID = int(get_env_var("GITHUB_APP_ID"))
+GITHUB_PRIVATE_KEY = get_env_var("GITHUB_PRIVATE_KEY")
+
 if GITHUB_PRIVATE_KEY.endswith(".pem"):
-    with open(GITHUB_PRIVATE_KEY, "r", encoding="utf-8") as key_file:
-        GITHUB_PRIVATE_KEY = key_file.read()
+    try:
+        with open(GITHUB_PRIVATE_KEY, "r", encoding="utf-8") as key_file:
+            GITHUB_PRIVATE_KEY = key_file.read()
+    except FileNotFoundError:
+        if LOGGING_LEVEL == "DEBUG":
+            pass
+        else:
+            raise
 
-OPENAI_API_KEY = get_env_var("OPENAI_API_KEY", default="")
+OPENAI_API_KEY = get_env_var("OPENAI_API_KEY")
 OPENAI_MODEL = get_env_var("OPENAI_MODEL", default="gpt-3.5-turbo")
 
 PROMPT_PATH = get_env_var("PROMPT_PATH", default="src/prompt.txt")
 with open(PROMPT_PATH, "r", encoding="utf-8") as prompt_file:
     PROMPT = prompt_file.read()
-
-LOGGING_LEVEL = get_env_var("LOGGING_LEVEL", default="DEBUG")
