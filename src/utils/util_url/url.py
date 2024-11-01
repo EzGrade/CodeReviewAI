4064109926caa48637ed2a974a1cb42fc7d35e60
Grade <aborecevskij94@gmail.com>
2024-11-01 14:53:39 +0200

    fix: regex

--- src/utils/util_url/url.py
+++ src/utils/util_url/url.py
@@ -6,13 +6,15 @@ logger = logger.get_logger(__name__)
 
 def extract_repo_from_url(repo_url: str) -> tuple:
     """
-    Extract owner and repo from GitHub repository URL
-    :param repo_url:
-    :return:
+    Extract owner, repo, and optionally branch from GitHub repository URL
+    :param repo_url: GitHub repository URL
+    :return: tuple containing owner and repo
     """
     logger.debug(f"Extracting owner and repo from URL: {repo_url}")
-    pattern = r'github\.com[:/](?P<owner>[^/]+)/(?P<repo>[^/]+?)(?:\.git)?$'
+
+    pattern = r'(?:https://github\.com/|git@github\.com:)(?P<owner>[^/]+)/(?P<repo>[^/.]+)'
     match = re.search(pattern, repo_url)
+
     if match:
         owner = match.group('owner')
         repo = match.group('repo')
