68788bfc25737290c4d0e2ab9b5dea04c38372c2
Grade <aborecevskij94@gmail.com>
2024-11-01 13:11:14 +0200

    add: logger

---
+++ src/services/openai_service.py
@@ -0,0 +1,41 @@
+from typing import List, Dict
+
+import openai
+
+import config
+import logger
+
+logger = logger.get_logger(__name__)
+
+
+class OpenAi:
+    def __init__(
+            self,
+            context: List[Dict[str, str]],
+            api_key: str = config.OPENAI_API_KEY,
+            model: str = config.OPENAI_MODEL
+    ):
+        self.context = context
+        self.api_key = api_key
+        self.model = model
+        self.client = self.get_openai_client()
+
+    def get_openai_client(self):
+        return openai.OpenAI(
+            api_key=self.api_key
+        )
+
+    def get_response(
+            self,
+    ) -> str:
+        """
+        Get response from OpenAI
+        :return response: OpenAI response
+        """
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=self.context
+        )
+        ai_message = response.choices[0].message.content
+        logger.info(f"AI response: {ai_message}")
+        return ai_message
