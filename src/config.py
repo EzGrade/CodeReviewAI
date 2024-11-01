a54e3969ea56b2439a4329aa7c4413b0c868b790
Grade <aborecevskij94@gmail.com>
2024-11-01 15:16:16 +0200

    add: redis variables

--- src/config.py
+++ src/config.py
@@ -21,3 +21,9 @@ OPENAI_MODEL = get_env_var("OPENAI_MODEL", default="gpt-3.5-turbo")
 PROMPT_PATH = get_env_var("PROMPT_PATH", default="src/prompt.txt")
 with open(PROMPT_PATH, "r", encoding="utf-8") as prompt_file:
     PROMPT = prompt_file.read()
+
+REDIS_HOST = get_env_var("REDIS_HOST", default="redis")
+REDIS_PORT = get_env_var("REDIS_PORT", default="6379")
+REDIS_PASSWORD = get_env_var("REDIS_PASSWORD", default="")
+REDIS_DB = get_env_var("REDIS_DB", default="0")
+REDIS_CACHE_EXPIRATION = int(get_env_var("REDIS_CACHE_EXPIRATION", default="3600"))
