e007ac261fa4a6d0c4b02a93f711caa788671024
Grade <aborecevskij94@gmail.com>
2024-11-02 16:38:10 +0200

    add: github token param

--- src/config.py
+++ src/config.py
@@ -6,18 +6,7 @@ from utils.util_environment.environment import get_env_var
 
 LOGGING_LEVEL = get_env_var("LOGGING_LEVEL", default="DEBUG")
 
-GITHUB_APP_ID = int(get_env_var("GITHUB_APP_ID"))
-GITHUB_PRIVATE_KEY = get_env_var("GITHUB_PRIVATE_KEY")
-
-if GITHUB_PRIVATE_KEY.endswith(".pem"):
-    try:
-        with open(GITHUB_PRIVATE_KEY, "r", encoding="utf-8") as key_file:
-            GITHUB_PRIVATE_KEY = key_file.read()
-    except FileNotFoundError:
-        if LOGGING_LEVEL == "DEBUG":
-            pass
-        else:
-            raise
+GITHUB_TOKEN = get_env_var("GITHUB_TOKEN")
 
 OPENAI_API_KEY = get_env_var("OPENAI_API_KEY")
 OPENAI_MODEL = get_env_var("OPENAI_MODEL", default="gpt-3.5-turbo")
